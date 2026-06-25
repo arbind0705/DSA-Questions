675c9ce5015820e4cfe3c04ab9a36a0f97c48cb2
Arbind <arbindmalava@gmail.com>
2026-06-25 21:22:20 +0530

    new input

---
+++ Array/FANDA.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        vector<int> missing;
+
+        sort(nums.begin(), nums.end());
+        int expected = 1;
+        int n = nums.size();
+
+        for (int num : nums) {
+            while (num > expected) {
+                missing.push_back(expected);
+                expected++;
+            }
+
+            if (expected == num) {
+                expected++;
+            }
+        }
+
+        while (expected <= n) {
+            missing.push_back(expected);
+            expected++;
+        }
+
+        return missing;
+    }
+};
+
+int main() {
+    vector<int> nums = {4, 3, 2, 7, 8, 2, 3, 1};
+
+    Solution sol;
+    vector<int> result = sol.findDisappearedNumbers(nums);
+
+    cout << "Missing numbers: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
